27686331c6601a72f5f1b008809cbc3f13162df1
Adityasharma0709 <138314085+Adityasharma0709@users.noreply.github.com>
2024-07-16 23:35:42 +0530

    Add files via upload

---
+++ assgn1/script1.js
@@ -0,0 +1,8 @@
+function activeHome() {
+    document.getElementById("home").classList.add("active");
+    document.getElementById("first").classList.remove("active");
+}
+function activecontact() {
+    document.getElementById("home").classList.remove("active");
+    document.getElementById("contact").classList.add("active");
+}
